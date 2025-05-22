5a828d4a1486801534edf4dac79f19bd152e1852
Raveena Mattu <Raveena.Mattu@triosstudent.com>
2025-05-22 08:31:22 -0400

    Edited database_error.php

--- database_error.php
+++ database_error.php
@@ -7,7 +7,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Student Manager - Home</title>
+  <title>Student Manager - Database Error</title>
   <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
