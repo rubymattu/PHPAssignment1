2591f3fd90e081d614548e1dfe85bc1c5249da7a
Raveena Mattu <Raveena.Mattu@triosstudent.com>
2025-05-22 00:10:49 -0400

    Edited index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Student Manager - Home</title>
-  <link rel="stylesheet" type="txt/css" href="css/main.css">
+  <link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
 <body>
   <?php
